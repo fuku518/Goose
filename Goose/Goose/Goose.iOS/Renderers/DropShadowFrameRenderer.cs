2da77f670cd6d12ed75b7d2209aa9edab7f5aab3
Kenji Fukumoto <fukuken1968@me.com>
2019-09-23 03:03:58 +0900

    add listview item's dropshadow custom renderer

---
+++ Goose/Goose/Goose.iOS/Renderers/DropShadowFrameRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using Goose.Controls;
+using Goose.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DropShadowFrame), typeof(DropShadowFrameRenderer))]
+namespace Goose.iOS.Renderers
+{
+    public class DropShadowFrameRenderer : FrameRenderer
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+
+            Layer.ShadowRadius = 4.0f;
+            Layer.ShadowColor = Color.Gray.ToCGColor();
+            Layer.ShadowOffset = new CGSize(2, 2);
+            Layer.ShadowOpacity = 0.4f;
+            Layer.ShadowPath = UIBezierPath.FromRect(Layer.Bounds).CGPath;
+            Layer.MasksToBounds = false;
+        }
+    }
+}
---
+++ Goose/Goose/Goose/Controls/DropShadowFrame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Goose.Controls
+{
+    /// <summary>
+    /// ドロップシャドウ付きのFrame
+    /// </summary>
+    /// <remarks>
+    /// カスタムレンダラーの対象を限定するためだけの継承。
+    /// </remarks>
+    public class DropShadowFrame : Frame
+    {
+    }
+}
