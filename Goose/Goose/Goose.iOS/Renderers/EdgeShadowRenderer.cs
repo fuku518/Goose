703795bb1616d4286d361ed8c2861a4b64f8eab2
Kenji Fukumoto <fukuken1968@me.com>
2019-09-23 04:25:31 +0900

    add edge shadow inside title and listview

---
+++ Goose/Goose/Goose.iOS/Renderers/EdgeShadowRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using Goose.Controls;
+using Goose.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EdgeShadow), typeof(EdgeShadowRenderer))]
+namespace Goose.iOS.Renderers
+{
+    public class EdgeShadowRenderer : BoxRenderer
+    {
+        private CAGradientLayer _shadowLayer { get; set; }
+
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+
+            if (_shadowLayer != null)
+            {
+                _shadowLayer.RemoveFromSuperLayer();
+                _shadowLayer = null;
+            }
+            _shadowLayer = new CAGradientLayer()
+            {
+                StartPoint = new CGPoint(0, 0),
+                EndPoint = new CGPoint(0, 1)
+            };
+            _shadowLayer.Frame = rect;
+            _shadowLayer.Colors = new CGColor[] {
+                Color.FromHex("#60000000").ToCGColor(),
+                Color.FromHex("#00000000").ToCGColor(),
+            };
+            Layer.InsertSublayer(_shadowLayer, 0);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_shadowLayer != null)
+            {
+                _shadowLayer.RemoveFromSuperLayer();
+                _shadowLayer = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Goose/Goose/Goose/Controls/EdgeShadow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Goose.Controls
+{
+    public class EdgeShadow : BoxView
+    {
+    }
+}
